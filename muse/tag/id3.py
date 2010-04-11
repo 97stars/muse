900f6e38fbdaf9c33680de7eab60e15c23a2ea8c
Tom Neudoerffer <tom.neudoerffer@gmail.com>
2010-04-10 21:02:06 -0400

    fixed size for long (>127 bytes) frames

--- muse/tag/id3.py
+++ muse/tag/id3.py
@@ -17,7 +17,7 @@ class ID3(dict):
         else: filedata = "" # filedata is blank if we're saving to a new file
         framedata = self.__pack_frames();
         with open(self.file, "wb") as f:
-            f.write(pack("<3s BB B",
+            f.write(pack(">3s BB B",
                          "ID3",
                          self.__version[0], self.__version[1],
                          0))
@@ -37,24 +37,31 @@ class ID3(dict):
             if frame_id == "TXXX":
                 desc = self[frame_id]["DESC"].encode("UTF-16")
                 text = self[frame_id]["TEXT"].encode("UTF-16")
-                frametext = pack("<B", 1) # encoding
-                frametext += pack("<%dsxx%ds" % (len(desc), len(text)),
+                frametext = pack(">B", 1) # encoding
+                frametext += pack(">%dsxx%ds" % (len(desc), len(text)),
                                   desc, text)
             else:
-                text = self[frame_id].encode("UTF-16")
-                frametext = pack("<B", 1) # encoding
-                frametext += pack("<%ds" % len(text), text)
+                if frame_id == "TYER":
+                    text = self[frame_id].encode("iso-8859-1")
+                    frametext = pack(">B", 0)
+                elif frame_id == "TRCK":
+                    text = self[frame_id].encode("iso-8859-1")
+                    frametext = pack(">B", 0)
+                else:
+                    text = self[frame_id].encode("UTF-16")
+                    frametext = pack(">B", 1) # encoding
+                frametext += pack(">%ds" % len(text), text)
             # start of header
-            framedata += pack("<4s", frame_id)
-            framedata += _pack_id3_size(len(frametext))
-            framedata += pack("<H", 0)
+            framedata += pack(">4s", frame_id)
+            framedata += pack(">I", len(frametext))
+            framedata += pack(">H", 0)
             # end of header
             framedata += frametext
         return framedata
 
 def _is_id3(fh):
     fh.seek(0, os.SEEK_SET)
-    ident = unpack("<3s", fh.read(3))[0]
+    ident = unpack(">3s", fh.read(3))[0]
     fh.seek(0, os.SEEK_SET)
     return ident == "ID3"
 
@@ -74,7 +81,7 @@ def _pack_id3_size(size):
         size >>= 7
         ret.append(size & 0x7F)
     ret.reverse()
-    return pack("<BBBB", *ret)
+    return pack(">BBBB", *ret)
 
 def _unpack_id3_size(packedsize):
     ret = packedsize[0]
