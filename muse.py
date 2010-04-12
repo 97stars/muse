ceefa8b50c291a112c67bcd919455e664bc2f84d
Tom Neudoerffer <tom.neudoerffer@gmail.com>
2010-04-12 13:04:06 -0400

    improved command line interface

--- muse.py
+++ muse.py
@@ -5,35 +5,34 @@ from optparse import OptionParser, make_option
 from muse import Muse
 from muse.tag.tag import AgnosticTags
 
-USAGE = "usage: %prog -d DIR -i GLOB -t EXTENSION [options]"
+USAGE = "usage: %prog [options] [INDIR [OUTDIR]]"
 
 OPTION_LIST = [
     make_option("-o", "--output", action="store",
                 type="string", dest="output", metavar="DIR"),
-    make_option("-d", "--directory", action="store",
-                type="string", dest="dir", metavar="DIR"),
     make_option("-i", "--input", action="store",
                 type="string", dest="input", metavar="GLOB"),
-    make_option("-t", "--out-type", action="store",
-                type="string", dest="type", metavar="EXTENSION"),
 ]
 
 def parseopts():
-    parser = OptionParser(option_list=OPTION_LIST)
+    parser = OptionParser(option_list=OPTION_LIST, usage=USAGE)
     (options, args) = parser.parse_args()
-    if not options.dir: parser.error("directory option is required")
     if not options.input: parser.error("input option is requred")
-    if not options.type: parser.error("type option is required")
-    if len(args) != 0: parser.error("I take no positional args!")
-    return options
+    if not options.output: parser.error("output option is required")
+    if len(args) > 2: parser.error("too many positional args!")
+    return (options, args)
 
 def main():
-    options = parseopts()
-    m = Muse(options.input, options.type)
-    if options.output:
-        m.go(unicode(options.dir), unicode(options.output))
+    options, args = parseopts()
+    i = "*.%s" % options.input
+    o = ".%s" % options.output
+    m = Muse(i, o)
+    if len(args) == 2:
+        m.go(unicode(args[0]), unicode(args[1]))
+    elif len(args) == 0:
+        m.go(u".")
     else:
-        m.go(unicode(options.dir))
+        m.go(unicode(args[0]))
 
 if __name__ == '__main__':
     main()
